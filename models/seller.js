901139e487ee086d869e0c87a5fb63b9d0dd944f
eduardobautistag <eduardo.bautista@delfosti.com>
2022-05-20 19:40:18 -0500

    seller in building

---
+++ models/seller.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const sellerSchema = Schema({
+    name: {
+        type: String,
+        required: [true,'Campo requerido: Nombre']
+    },
+    lastName: {
+        type: String,
+        required: [true,'Campo requerido: Apellido']
+    },
+    password: {
+        type: String,
+        required: [true,'Campo requerido: Contraseña']
+    },
+    role: {
+        type: Number,
+        required: [true,'Campo requerido: Rol']
+    },
+    createAt: {
+        type: Date,
+        required: [true,'Requerimiento interno: Fecha de creacion']
+    },
+    phone: {
+        type: String,
+        required: [true,'Campo requerido: Telefono']
+    },
+    email: {
+        type: String,
+        required: [true,'Campo requerido: Email']
+    },
+    state: {
+        type: String,
+        default:  [true,'Requerimiento interno: Estado']
+    },
+    supervisor:[{
+        _id:{
+            type: String,
+            required: [false,'Campo requerido: Id de Supervisor']
+        } 
+    }]
+})
+
+
+module.exports = mongoose.model('Seller', customerSchema);
